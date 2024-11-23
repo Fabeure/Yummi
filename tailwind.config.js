e0d3d1d84975374ddf2d861c814ca2ea8c3b31ec
Sabaa-Abdennabi <saba.abdennebi@insat.ucar.tn>
2024-11-23 23:53:40 +0100

    added footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
     fontFamily:{
       'playfair':['Playfair Display', 'serif'],
       'inter':['Inter', 'sans-serif'],
+      'playfair':['Playfair','serif']
     },
     extend: {},
   },
