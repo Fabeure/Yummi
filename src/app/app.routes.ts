f421355a99ecf60d79ea6748984396e3c4132594
Farah Bassoumi <bassoumiifarah@gmail.com>
2024-12-14 01:58:24 +0100

    add app-product-list in the profile page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,7 +11,7 @@ export const routes: Routes = [
   {
     path: 'test',
     component: TestSearchComponent,
-  }
+  },
   {
     path: 'profile',
     component: ProfileComponent,
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,15 +1,17 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { fetchItemsFromApi, ProductItem } from '../../shared/mock-data/mockData';
+import { ProductListComponent } from "../../components/product-list/product-list.component";
 
 
  @Component({
   selector: 'app-profile',
-  imports: [FormsModule],
+  imports: [FormsModule, ProductListComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
   standalone: true,
 })
-export class ProfileComponent  {
+export class ProfileComponent   implements OnInit {
 
     fullName = 'Saboua Abd';
     username = 'Miller';
@@ -39,5 +41,41 @@ export class ProfileComponent  {
       }
     }
   
+    products: ProductItem[] = [];
+    page = 1;
+    pageSize = 8;
+    total = 0;
+  
+    get showLoadMore(): boolean {
+      return this.products.length < this.total;
+    }
+    async ngOnInit(): Promise<void> {
+      await this.loadPage(this.page);
+    }
+  
+    constructor(private cdr: ChangeDetectorRef) {}
+    
+    
+    private async loadPage(page: number) {
+      const { total, items } = await fetchItemsFromApi(page, this.pageSize);
+      this.total = total;
+  
+      
+  
+      if (page === 1) {
+        // Initial load, replace items
+        this.products = items;
+      } else {
+        // Append to existing list
+        this.products = [...this.products, ...items];
+      }
+      this.cdr.detectChanges(); // Trigger change detection to update view
+    }
+  
+    handleLoadMore() {
+      this.page++;
+      this.loadPage(this.page);
+    }
+
   }
   
