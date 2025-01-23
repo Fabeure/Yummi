2bfe1a678b5f0cfa23ec79f6b7e1f63545ed5af0
jihen <jihenmkhinini4070@gmail.com>
2025-01-23 16:48:13 +0200

    categories update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -144,10 +144,30 @@ export class HomeComponent {
     },
   ];
   categories = [
-    { name: 'Pizza', image: 'categories/pizza.jpg' },
-    { name: 'Pasta', image: 'categories/pasta.jpg' },
-    { name: 'Smoothie', image: 'categories/smoothie.webp' },
-    { name: 'Vegan', image: 'categories/vegan.jpg' },
-    { name: 'Dessert', image: 'categories/dessert.jpg' }
+    { name: 'Main Course', image: 'categories/pasta.jpg' },
+    { name: 'Side Dish', image: 'categories/side-dish.jpg' },
+    { name: 'Dessert', image: 'categories/dessert.jpg' },
+    { name: 'Appetizer', image: 'categories/Appetizer.jpg' },
+    { name: 'Salad', image: 'categories/vegan.jpg' },
+    { name: 'Bread', image: 'categories/bread.webp' },
+    { name: 'Breakfast', image: 'categories/breakfast.jpg' },
+    { name: 'Soup', image: 'categories/soup.jpg' },
+    { name: 'Beverage', image: 'categories/beverage.jpg' },
+    { name: 'Sauce', image: 'categories/sauce.avif' },
+    { name: 'Marinade', image: 'categories/marinade.jpg' },
+    { name: 'Finger Food', image: 'categories/finger-food.jpeg' },
+    { name: 'Snack', image: 'categories/snack.jpg' },
+    { name: 'Drink', image: 'categories/smoothie.webp' }
   ];
+  visibleCategories = this.categories.slice(0, 5);
+  rotateCategories() {
+    // Remove the first category and add it to the end
+    const firstCategory = this.categories.shift();
+    if (firstCategory) {
+      this.categories.push(firstCategory);
+    }
+
+    // Update the visible categories
+    this.visibleCategories = this.categories.slice(0, 5);
+  }
 }
