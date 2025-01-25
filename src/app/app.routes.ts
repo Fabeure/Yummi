5d1154ac5537ac84a6bea586d43c26dc91b3bcfb
Fabeure <sazaouzi@gmail.com>
2025-01-25 23:21:54 +0100

    add auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { CategoriesComponent } from './pages/categories/categories.component';
 import { APP_ROUTES } from '../config/routes.config';
 import { RecipeResolver } from './resolvers/recipe.resolver';
 import { resolve } from 'node:path';
+import { LoginGuard } from './guards/route.guard';
 
 export const routes: Routes = [
   {
@@ -20,6 +21,7 @@ export const routes: Routes = [
   {
     path: APP_ROUTES.profile,
     component: ProfileComponent,
+    canActivate: [LoginGuard]
   },
   {
     path: APP_ROUTES.recipe,
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+
+export const environment = {
+  production: false,
+  apiKey: 'saber',
+  apiUrl: 'https://studyfiedbackendtest.onrender.com'
+};
