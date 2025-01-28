e73bbec034b3b1c34fcf8fb068f45e8f5b9bdc1b
Arij Thabet <arijthabet01@gmail.com>
2025-01-28 10:52:55 +0100

    corrected links of mealplans in home and planner

---
+++ src/constants/diet.const.ts
@@ -0,0 +1,26 @@
+export const DIET_IMAGES: { [key: string]: string } = {
+    ketogenic: 'diet/keto.jpg',
+    vegetarian: 'diet/vegetarian.png',
+    vegan: 'diet/vegan.jpg',
+    paleo: 'diet/paleo.jpg',
+    pescetarian: 'diet/pescatarian.jpg',
+    glutenFree: 'diet/glutenFree.png',
+    dairyFree: 'diet/dairyFree.jpg',
+    // Add more diets and corresponding image paths here
+  };
+  
+
+  export const DIETS = [
+    { name: 'Whole30', key: 'whole30', icon: 'icon/meal.png', image: 'diet/whole30.jpg' },
+    { name: 'Low FODMAP', key: 'lowFodmap', icon: 'icon/diet1.png', image: 'diet/lowfodmap.png' },
+    { name: 'Primal', key: 'primal', icon: 'icon/diet.png', image: 'diet/primal.webp' },
+    { name: 'Paleo', key: 'paleo', icon: 'icon/paleo-diet.png', image: 'diet/paleo.jpg' },
+    { name: 'Pescetarian', key: 'pescetarian', icon: 'icon/nutrition-plan.png', image: 'diet/pescatarian.jpg' },
+    { name: 'Vegan', key: 'vegan', icon: 'icon/diet-food.png', image: 'diet/vegan.jpg' },
+    { name: 'Ovo-Vegetarian', key: 'ovoVegetarian', icon: 'icon/healthy-food.png', image: 'diet/ovovegetarian.jpg' },
+    { name: 'Lacto-Vegetarian', key: 'lactoVegetarian', icon: 'icon/salad.png', image: 'diet/lactovegetarian.jpg' },
+    { name: 'Vegetarian', key: 'vegetarian', icon: 'icon/salad.png', image: 'diet/vegetarian.png' },
+    { name: 'Ketogenic', key: 'ketogenic', icon: 'icon/keto.png', image: 'diet/keto.jpg' },
+    { name: 'Gluten Free', key: 'glutenFree', icon: 'icon/gluten-free.png', image: 'diet/glutenFree.png' },
+  ];
+  
